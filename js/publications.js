22e968aebbb6bba48988ae807b6534b33a1e48a0
Maxime Puys <Maxime.Puys@cea.fr>
2022-04-13 16:48:34 +0200

    Cleanup

--- js/publications.js
+++
@@ -1,9 +0,0 @@
-function hideshow(which){
-    if (!document.getElementById)
-        return
-
-    if (which.style.display=="block")
-        which.style.display="none"
-    else
-        which.style.display="block"
-}
