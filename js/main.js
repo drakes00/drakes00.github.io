68d24100e8c61902509ce906dec9d08e48c0cd04
GitLab CI <ci@gitlab.com>
2025-10-25 12:58:59 +0000

    Deploy from GitLab CI - 2025-10-25_12:58:59

--- js/main.js
+++ js/main.js
@@ -29,28 +29,14 @@
 						// Clear active and lock scrollzer until scrolling has stopped
 							$nav_a
 								.removeClass('active')
-								.addClass('scrollzer-locked');
+
 
 						// Set this link to active
 							t.addClass('active');
 
 					});
 
-			// Initialize scrollzer.
-				var ids = [];
 
-				$nav_a.each(function() {
-
-					var href = $(this).attr('href');
-
-					if (href[0] != '#')
-						return;
-
-					ids.push(href.substring(1));
-
-				});
-
-				$.scrollzer(ids, { pad: 200, lastHack: true });
 
 	});
 
